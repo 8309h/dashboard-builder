bfa817fcdc7e77fae6ca232bada3eff4a89aab40
harshal <harshalwagh201718@gmail.com>
2026-06-24 14:54:47 +0530

    Added text widget factory

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,44 @@
-console.log("Dashboard Builder Started");
+const dashboard = document.getElementById("dashboard");
+
+const addTextBtn = document.getElementById("addTextBtn");
+
+let widgetCounter = 1;
+
+function createTextWidget() {
+
+      const widget = document.createElement("div");
+
+      widget.classList.add("widget");
+
+      widget.dataset.id = `widget-${widgetCounter++}`;
+
+      widget.dataset.type = "text";
+
+      widget.style.left = "100px";
+      widget.style.top = "100px";
+
+      widget.innerHTML = `
+        <div class="widget-header">
+
+            <span>
+                Text Widget
+            </span>
+
+            <button class="widget-menu">
+                ⋮
+            </button>
+
+        </div>
+
+        <div
+            class="widget-content text-widget-content"
+            contenteditable="true"
+        >
+            Start typing here...
+        </div>
+    `;
+
+      dashboard.appendChild(widget);
+}
+
+addTextBtn.addEventListener("click", createTextWidget);
